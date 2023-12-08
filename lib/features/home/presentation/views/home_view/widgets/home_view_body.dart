a52fb25e3b3432e4121dcb04d5e1ad2b06dbb4db
moElsmokhraty <melsmokhraty@yahoo.com>
2023-12-08 19:58:36 +0200

    Finish Home View UI

--- lib/features/home/presentation/views/home_view/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/home_view/widgets/home_view_body.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:opi_se/core/utils/styling/styles.dart';
 import 'package:opi_se/core/functions/validate_text.dart';
+import 'package:opi_se/core/widgets/buttons/auth_button.dart';
 import 'package:opi_se/core/widgets/text_fields/auth_text_field.dart';
-
-import 'recommendation_item.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.sizeOf(context).height;
     return Padding(
       padding: EdgeInsets.only(right: 16.w, left: 16.w, top: 16.h),
       child: Column(
@@ -28,10 +29,62 @@ class HomeViewBody extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: ListView.builder(
+            child: ListView(
               physics: const BouncingScrollPhysics(),
-              itemCount: 5,
-              itemBuilder: (context, index) => const RecommendationItem(),
+              children: [
+                SizedBox(height: screenHeight * 0.025),
+                Text.rich(
+                  textScaler: const TextScaler.linear(1.1),
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'With',
+                        style: AppStyles.textStyle24.copyWith(
+                          fontSize: 30.sp,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      TextSpan(
+                        text: ' Opi Se ',
+                        style: AppStyles.textStyle24.copyWith(
+                          color: const Color(0xFF036666),
+                          fontSize: 30.sp,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      TextSpan(
+                        text: 'You\nCan Find The\nPerfect Study Partner',
+                        style: AppStyles.textStyle24.copyWith(
+                          fontSize: 30.sp,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.03),
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur elit, sed nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velitesse cillum dolore eu fugiat nulla',
+                  style: AppStyles.textStyle24.copyWith(
+                    fontSize: 15.sp,
+                    color: Colors.black.withOpacity(0.6),
+                    overflow: TextOverflow.visible,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.05),
+                AuthButton(
+                  text: 'Get a new Brain Buddy',
+                  onPressed: () {},
+                  backColor: const Color(0xFF036666),
+                  textColor: Colors.white,
+                ),
+                Image.asset(
+                  'assets/images/home_asset.png',
+                  width: double.infinity,
+                  height: 225.h,
+                ),
+              ],
             ),
           ),
         ],
@@ -39,5 +92,3 @@ class HomeViewBody extends StatelessWidget {
     );
   }
 }
-
-
